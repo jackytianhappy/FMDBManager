ff0f2ab0f0ea23d34bf808965f32ebd5750ac131
jackytianhappy <1142899950@qq.com>
2016-11-07 18:04:28 +0800

    初始化项目

---
+++ FMDBManager/FMDBManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FMDBManager
+//
+//  Created by Jacky on 16/11/7.
+//  Copyright © 2016年 jacky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ FMDBManager/FMDBManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FMDBManager
+//
+//  Created by Jacky on 16/11/7.
+//  Copyright © 2016年 jacky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
